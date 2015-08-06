eb22133bc47ceb5d473503a8a517e57f5313c2b9
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2015-08-06 15:31:36 -0400

    adding more comments

--- public/scripts/visualizations/bargraph.js
+++ public/scripts/visualizations/bargraph.js
@@ -1,111 +1,146 @@
 ///////////////////////////////////
 /////////BAR TEMPLATE//////////////
-//////////////////////////////////
+///////////////////////////////////
+
 console.log('loading bargraph.js');
 var makeBarGraph = function(data, charParams, svgParams) {
-  // Graph dimensions
-  var width = svgParams.width || 1000,
-      height = svgParams.height || 600,
-      padding = svgParams.padding || 200;
 
-  var x = d3.scale.ordinal().rangeRoundBands([0, width], [0.09]);
-  var y = d3.scale.linear().range([height, 0]);
+    //////////////////////////////////////
+    //////// SVG PARAMETERS //////////////
+    //////////////////////////////////////
 
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
+    var width = svgParams.width || 1000,
+        height = svgParams.height || 600,
+        padding = svgParams.padding || 200;
 
-  var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+    /////////////////////////////////////
+    //////// D3 PARAMETERS //////////////
+    /////////////////////////////////////
 
-  //colors
-  var color = d3.scale.category20();
-  var $title = $('#title');
-  $title.text(data.title);
-  //
-  // var maxYears = []
-  // var years = [9,11,13,15,17]; //so this is different depending on the data set :(
-  //   for(q = 0; q < years.length; q++){
-  //     maxYears.push(d3.max(data.data, function(d) { return parseInt(d[years[q]]);}));
-  //   }
-  // console.log(maxYears +'maxYears');
-  // var year = years[0];
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], [0.09]);
+    var y = d3.scale.linear().range([height, 0]);
 
-    // Loading data
-    //d3 max is treating numbers as strings, so we convert below
-    // var maxY = d3.max(maxYears, function(d) { return d;})
-    // console.log(maxY);
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
-    // x.domain(data.data.map(function(d) { return d[8]; }));
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+
+      //colors
+    var color = d3.scale.category20();
+
+    ///////////////////////////////////////////
+    //////// ADDING TITLE TO DOM //////////////
+    /////////////////////////////////////////// TODO FIXME
+
+    var $title = $('#title');
+    $title.text(data.title);
+
+
+    ///////////////////////////////////////////
+    //////// XAXIS YAXIS DOMAINS //////////////
+    ///////////////////////////////////////////
+
     x.domain(data.abuses);
     y.domain([0,data.maxYValue +100]);
 
 
-   function drawBars(){
-      $('svg').remove();
-        //Append our SVG to the HTML body
-    var svg = d3.select("body").append("svg")
-      .attr("width", width + padding * 2)
-      .attr("height", height + padding * 2)
-      .append("g")
-      .attr("transform", "translate(" + padding + "," + padding + ")");
+    function drawBars(){
+        ///////////////////////////////////////////////
+        //////// REMOVE THE SVG AND BUTTONS ///////////
+        ///////////////////////////////////////////////
 
-      svg.selectAll("thisdoesnotmatter")
-        .data(data.data) //love this line!
-        .enter()
-        .append("rect")
-        .style('fill',function(d, i){ return color(d); })
-        .on('mouseover', function(d) {
-          d3.select(this)
-          .style('fill', 'black');
-          var myText = $(this).attr('id');
-            var x = event.pageX - this.offsetLeft;
-            var y = event.pageY - this.offsetTop;
-            $('.blurb').css('visibility', 'visible').css('margin-left', x-50).css('margin-top', y-150).fadeIn('slow').text(myText);
-        })
-        .on('mouseout', function(d){
-          d3.select(this)
-          .style('fill',function(d){ return color(d); });
-          setTimeout(function(){
-              $('.blurb').fadeOut('slow');
-            },1500);
-        })
-        .attr("x", function(d) { return x(d[0]); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d, i) { return y(d[1]);  })
-        .style('opacity', 0)
-        .transition().delay(function (d,i){ return i * 100;})
-        .duration(300)
-        .style('opacity', 1)
-        .attr("height", function(d, i) { return height - y(d[1]); })
-        .attr('id', function(d){ return d[8]})
-        .attr('class', 'bars');
+        $('svg').remove();
+        $('#buttons button').remove();
+        $('#keys *').remove();
 
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis)   //calls the xAxis var we made up above
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("x", -25)
-        .attr("transform", "rotate(-60)");
+        ///////////////////////////////////////////////
+        //////// APPEND SVG TO HTML BODY //////////////
+        ///////////////////////////////////////////////
 
-      svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .selectAll("text");
-   }
+        var svg = d3.select("body").append("svg")
+          .attr("width", width + padding * 2)
+          .attr("height", height + padding * 2)
+          .append("g")
+          .attr("transform", "translate(" + padding + "," + padding + ")");
+
+        ///////////////////////////////////////////////////////
+        //////// APPEND BARS PLUS HOVER HANDLERS //////////////
+        ///////////////////////////////////////////////////////
+
+        svg.selectAll("thisdoesnotmatter")
+          .data(data.data) //love this line!
+          .enter()
+          .append("rect")
+          .style('fill',function(d, i){ return color(d); })
+          .on('mouseover', function(d) {
+            d3.select(this)
+            .style('fill', 'black');
+            var myText = $(this).attr('id');
+              var x = event.pageX - this.offsetLeft;
+              var y = event.pageY - this.offsetTop;
+              $('.blurb').css('visibility', 'visible').css('margin-left', x-50).css('margin-top', y-150).fadeIn('slow').text(myText);
+          })
+          .on('mouseout', function(d){
+            d3.select(this)
+            .style('fill',function(d){ return color(d); });
+            setTimeout(function(){
+                $('.blurb').fadeOut('slow');
+              },1500);
+          })
+          .attr("x", function(d) { return x(d[0]); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d, i) { return y(d[1]);  })
+          .style('opacity', 0)
+          .transition().delay(function (d,i){ return i * 100;})
+          .duration(300)
+          .style('opacity', 1)
+          .attr("height", function(d, i) { return height - y(d[1]); })
+          .attr('id', function(d){ return d[8]})
+          .attr('class', 'bars');
+
+          ///////////////////////////////////////////////////
+          //////// XAXIS LABELS AND TRANSFORMS //////////////
+          ///////////////////////////////////////////////////
+
+
+          svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(xAxis)   //calls the xAxis var we made up above
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("x", -25)
+            .attr("transform", "rotate(-60)");
+
+          svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .selectAll("text");
+
+    }
+
+
    drawBars();
-        //making the year buttons
+
+   //////////////////////////////////////////////////
+   //////// APPEND YEAR BUTTONS TO DOM //////////////
+   //////////////////////////////////////////////////
+
       $buttonDiv = $('#buttons');
       for (var yr = 2005; yr <= 2009; yr++ ) {
         $button = $('<button>').attr('id', yr).text(yr);
         $buttonDiv.append($button);
       }
         var $buttons = $('button');
-      //autoplay
+   
+   ///////////////////////////////////////////////
+   //////// AUTOPLAY THROUGH YEARS  //////////////
+   ///////////////////////////////////////////////
+
       var maxLoops = $buttons.length-1;
           var counter = 0;
           (function next() {
@@ -121,7 +156,10 @@ var makeBarGraph = function(data, charParams, svgParams) {
                   next();
               }, 5000);
           })();
-       //end of autoplay
+    ///////////////////////////////////////////////
+    ////////  BUTTON CLICK HANDLERS  //////////////
+    ///////////////////////////////////////////////
+
       $.each($buttons, function(i, val) {
         $(val).click(function() {
           year = data.years[i];
@@ -131,32 +169,37 @@ var makeBarGraph = function(data, charParams, svgParams) {
       });
   }
 
-  var adapterForDistributionOfAbuseAllegationsToBarChart = function(data) {
-  var result = {};
-  result.data = [];
-  var keys = Object.keys(data.data[0]).sort();
-  result.years = ["2005", "2006", "2007", "2008", "2009"];
-  result.abuses = [];
-  var yValues = [];
-  for (var i = 0; i < data.data.length - 1; i++) {
-    var currentAbuse = data.data[i];
+  /////////////////////////////////////////////////////////////////////
+  //////// ADAPTER FOR DISTRIBUTION OF ABUSE ALLEGATIONS //////////////
+  /////////////////////////////////////////////////////////////////////
 
-    var currentResult = [];
-    for (var j = 0; j < keys.length; j++) {
-      if (j < 10) {
-        currentResult.push(parseInt(currentAbuse[keys[j]]));
-        if (j < 4) {
-          yValues.push(parseInt(currentAbuse[keys[j]]));
+
+  var adapterForDistributionOfAbuseAllegationsToBarChart = function(data) {
+    var result = {};
+    result.data = [];
+    var keys = Object.keys(data.data[0]).sort();
+    result.years = ["2005", "2006", "2007", "2008", "2009"];
+    result.abuses = [];
+    var yValues = [];
+    for (var i = 0; i < data.data.length - 1; i++) {
+      var currentAbuse = data.data[i];
+
+      var currentResult = [];
+      for (var j = 0; j < keys.length; j++) {
+        if (j < 10) {
+          currentResult.push(parseInt(currentAbuse[keys[j]]));
+          if (j < 4) {
+            yValues.push(parseInt(currentAbuse[keys[j]]));
+          }
+        } else {
+          result.abuses.unshift(currentAbuse[keys[j]]);
+          currentResult.unshift(currentAbuse[keys[j]]);
         }
-      } else {
-        result.abuses.unshift(currentAbuse[keys[j]]);
-        currentResult.unshift(currentAbuse[keys[j]]);
       }
+      result.data.push(currentResult);
     }
-    result.data.push(currentResult);
-  }
-  result.maxYValue = d3.max(yValues, function(d) {return d;});
-  result.title = "Allegations of Abuse of Authority by Police";
-  console.log(result);
-  return result;
+    result.maxYValue = d3.max(yValues, function(d) {return d;});
+    result.title = "Allegations of Abuse of Authority by Police";
+    console.log(result);
+    return result;
   }
