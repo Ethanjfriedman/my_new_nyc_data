c0c64f1c924db1ae4090541d7506117ad1ac29d7
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2015-08-06 15:10:07 -0400

    adding more comments

--- public/scripts/visualizations/timeseries.js
+++ public/scripts/visualizations/timeseries.js
@@ -2,23 +2,37 @@
 /////////MULTI LINE TEMPLATE//////////////
 /////////////////////////////////////////
 console.log('loading timeseries.js');
+
 var makeTimeseries = function(data, chartParams, svgParams) {
+
+  ////////////////////////////////////////////////////////////////////////////
+  //// clearing out existing SVG elements as well as keys and buttons ////////
+  ////////////////////////////////////////////////////////////////////////////
+
+  $('svg').remove();
+  $('#buttons button').remove();
+  $('#keys *').remove();
 
   //////////////////////////////////////
   ////////// SVG VARIABLES /////////////
   //////////////////////////////////////
 
-  //clearing out existing SVG elements as well as keys and buttons
-  $('svg').remove();
-  $('#buttons button').remove();
-  $('#keys *').remove();
-
   var margin = svgParams.margin || {top: 20, right: 20, bottom: 30, left: 50},
       width = svgParams.width || 960 - margin.left - margin.right,
       height = svgParams.height || 500 - margin.top - margin.bottom;
 
+  
+  /////////////////////////////////////////////
+  ////////// setting d3 variables /////////////
+  /////////////////////////////////////////////
+
+  //color
+  ////////////////////////////////////////////
 
   var color = d3.scale.category20();
+
+  //scaling
+ ////////////////////////////////////////////
 
   var x = d3.time.scale()
       .range([0, width]);
@@ -34,10 +48,16 @@ var makeTimeseries = function(data, chartParams, svgParams) {
       .scale(y)
       .orient("left");
 
+ //line
+////////////////////////////////////////////
+
   var line = d3.svg.line()
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.value); });
 
+
+ //appending the whole svg
+ ////////////////////////////////////////////
 
   var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -45,8 +65,17 @@ var makeTimeseries = function(data, chartParams, svgParams) {
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+  ////////////////////////////////////////////
+  ////////// adding title to DOM /////////////
+  ////////////////////////////////////////////
+
   var $title = $('#title');
   $title.text(data.title);
+
+  /////////////////////////////////////////////////////////////////
+  ////////// finding all the max values for each year /////////////
+  /////////////////////////////////////////////////////////////////
 
   var allSeries = data.allSeries;
   var dates = data.dates;
@@ -61,6 +90,8 @@ var makeTimeseries = function(data, chartParams, svgParams) {
   y.domain(d3.extent([minVal, maxVal]));
 
   //making the x-axis
+  ////////////////////////////////////////////
+
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -71,6 +102,8 @@ var makeTimeseries = function(data, chartParams, svgParams) {
       .attr("transform", "rotate(-60)");
 
   // text label for the x axis
+  ////////////////////////////////////////////
+
   svg.append("text")
         .attr("transform", "translate(" + ((width + margin.right + margin.left) / 2) + " ," + (height + margin.bottom +30) + ")")
         .style("text-anchor", "middle")
@@ -81,12 +114,16 @@ var makeTimeseries = function(data, chartParams, svgParams) {
     //     .style("text-anchor", "middle")
     //     .text("Year");
 
+
   //making the y-axis
+  ////////////////////////////////////////////
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis);
 
   //label for the y-axis
+  ////////////////////////////////////////////
+
   svg.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", -50)
@@ -95,63 +132,73 @@ var makeTimeseries = function(data, chartParams, svgParams) {
         .style("text-anchor", "middle")
         .text("Number of incidents");
 
-    //graphing the data
-    for (var i = 0; i < allSeries.length; i++) {
+  //graphing the data
+  ////////////////////////////////////////////
 
-      svg.append("path")
-        .datum(allSeries[i])
-        .attr('opacity',0)
-        .attr('id', 'allSeries[i][0].series')
-        .on('mouseover', function(d) {
-        d3.select(this).attr('stroke-dasharray',"5,5").attr('stroke-width', '5px');
-          var myText = $(this).attr('class').split(' ');
+  for (var i = 0; i < allSeries.length; i++) {
 
-          myText.shift();
-          myText = myText.join('');
-          $(document).mousemove( function(e) {
-             mouseX = e.pageX;
-             mouseY = e.pageY;
-          });
-          $('.blurb').css('visibility', 'visible').css('top', mouseY).css('left', mouseY).fadeIn('slow').text(myText);
+    svg.append("path")
+      .datum(allSeries[i])
+      .attr('opacity',0)
+      .attr('id', 'allSeries[i][0].series')
+      .on('mouseover', function(d) {
+      d3.select(this).attr('stroke-dasharray',"5,5").attr('stroke-width', '5px');
+        var myText = $(this).attr('class').split(' ');
 
-        })
-        .on('mouseout', function(d){
-          that = this;
-          setTimeout(function(){
-            d3.select(that).attr('stroke-dasharray',"none").attr('stroke-width', '2px');
-            $('.blurb').fadeOut('slow');
-          },1000);
-        })
-        .transition().delay(function (d,i){ return i * 1000;}).duration(1000)
-        .attr("class", "line " + allSeries[i][0].series)
-        .attr('opacity',1)
-        .attr("d", line)
-        .attr('stroke', function(d){ return color(i); });
+        myText.shift();
+        myText = myText.join('');
+        $(document).mousemove( function(e) {
+           mouseX = e.pageX;
+           mouseY = e.pageY;
+        });
+        $('.blurb').css('visibility', 'visible').css('top', mouseY).css('left', mouseY).fadeIn('slow').text(myText);
 
-      //adding points
-        svg.selectAll("dot")
-        .data(allSeries[i])
-        .enter().append("circle")
-        .attr('opacity', 0)
-        .attr('id',function(d) {return (d.value); })
-        .on('mouseover', function() {
-          var valueText = ($(this).attr('id')).slice(0,6);
-          $('.blurb').css('visibility', 'visible').css('margin-left', x-50).css('margin-top', y-150).fadeIn('slow').text(valueText);
-        })
-        .on('mouseout', function(d) {
-          that = this;
-          setTimeout(function(){
-            $('.blurb').fadeOut('slow');
-          },1000)
-        })
-        .transition().delay(function (d,i){ return i * 200;}).duration(100)
-        .attr("r", 3)
-        .attr('opacity',1)
-        .attr("cx", function(d) { return x(d.date); })
-        .attr("cy", function(d) { return y(d.value); })
-        .attr('fill',function(d){ return color(i); });
-    }
+      })
+      .on('mouseout', function(d){
+        that = this;
+        setTimeout(function(){
+          d3.select(that).attr('stroke-dasharray',"none").attr('stroke-width', '2px');
+          $('.blurb').fadeOut('slow');
+        },1000);
+      })
+      .transition().delay(function (d,i){ return i * 1000;}).duration(1000)
+      .attr("class", "line " + allSeries[i][0].series)
+      .attr('opacity',1)
+      .attr("d", line)
+      .attr('stroke', function(d){ return color(i); });
+
+    //adding points
+    ////////////////////////////////////////////
+
+      svg.selectAll("dot")
+      .data(allSeries[i])
+      .enter().append("circle")
+      .attr('opacity', 0)
+      .attr('id',function(d) {return (d.value); })
+      .on('mouseover', function() {
+        var valueText = ($(this).attr('id')).slice(0,6);
+        $('.blurb').css('visibility', 'visible').css('margin-left', x-50).css('margin-top', y-150).fadeIn('slow').text(valueText);
+      })
+      .on('mouseout', function(d) {
+        that = this;
+        setTimeout(function(){
+          $('.blurb').fadeOut('slow');
+        },1000)
+      })
+      .transition().delay(function (d,i){ return i * 200;}).duration(100)
+      .attr("r", 3)
+      .attr('opacity',1)
+      .attr("cx", function(d) { return x(d.date); })
+      .attr("cy", function(d) { return y(d.value); })
+      .attr('fill',function(d){ return color(i); });
+  }
 }
+
+
+///////////////////////////////////////////////
+/////////// ADAPTER FOR FIREARMS  /////////////
+///////////////////////////////////////////////
+
 
 var adapterForFirearmsToTimeseries = function(data) {
   var result = {}; //this object will be returned with the necessary data to graph the timeseries
