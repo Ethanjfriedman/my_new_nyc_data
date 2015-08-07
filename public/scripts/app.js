cd2eadee6f59efd0808b6fe0ff8802c52156cffa
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2015-08-07 13:22:35 -0400

    ok had a small heartattack all should be well now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,23 +13,15 @@ app.controller('visualizationController', ['$http', function($http) {
   var controller = this;
   this.makePieChart = makePieChart;
 
-<<<<<<< HEAD
+  this.formData={};
   
 
 
   this.getPieChartData = function () {
     console.log(controller.formData.chartParams);
     $http.get(controller.formData.dataURL)
-=======
-  //get data for and draw a pie chart
-  this.getPieChartData = function (dataURL) {
-    $http.get(dataURL)
->>>>>>> 65f0262063f718242ba86565d23200d86e957be5
       .then(function (dataset) {
-        controller.makePieChart( dataset, {
-          yearType:'2012',
-           dataType: 'firearms'
-        }, {} );
+        controller.makePieChart( dataset, controller.formData.chartParams, {} );
       });
   };
 
